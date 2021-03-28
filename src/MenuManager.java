68241103f80f59f496a4ca912497883fe97c6bcb
OhJunhyeok9912 <80148751+OhJunhyeok9912@users.noreply.github.com>
2021-03-28 17:30:05 +0900

    Update MenuManager.java
    함수를 이용한 메뉴 실행 구현

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -5,81 +5,39 @@ public class MenuManager {
     
 	public static ArrayList<String> AddNotice() {
 	 ArrayList<String> addNotice = new ArrayList<String>(); // 총 26개의 문자열이 저장됨. 
-     Scanner input = new Scanner(System.in);
-     Scanner input1 = new Scanner(System.in);
-     System.out.println("공고 넘버: (순서대로 입력하세요.)"); 
-   	 String NoticeNumber = input.next();  
-   	 addNotice.add("공고 넘버: "+NoticeNumber); // 1번
-     System.out.println("공고제목: ");
-   	 String NoticeTitle = input1.nextLine();  
-   	 addNotice.add("공고제목: "+NoticeTitle); //2번
+         Scanner input = new Scanner(System.in);
+         Scanner input1 = new Scanner(System.in);
+         System.out.println("공고 넘버: (순서대로 입력하세요.)"); String NoticeNumber = input.next(); addNotice.add("공고 넘버: "+NoticeNumber); // 1번
+         System.out.println("공고제목: "); String NoticeTitle = input1.nextLine();  addNotice.add("공고제목: "+NoticeTitle); //2번
    	 System.out.println("근무지 정보");   
-   	 System.out.println("회사명: ");
-   	 String CompanyName = input1.nextLine();
-   	 addNotice.add("근무지 정보"); // 3번
+   	 System.out.println("회사명: "); String CompanyName = input1.nextLine(); addNotice.add("근무지 정보"); // 3번 
    	 addNotice.add("회사명: "+CompanyName); // 4번
-   	 System.out.println("근무지 위치: ");
-   	 String location = input1.nextLine();
-   	 addNotice.add("근무지 위치: "+location); // 5번
+   	 System.out.println("근무지 위치: "); String location = input1.nextLine(); addNotice.add("근무지 위치: "+location); // 5번
    	 System.out.println("모집조건");
-   	 System.out.println("모집기간: ");
-   	 String period = input1.nextLine();  
-   	addNotice.add("모집조건");  // 6번
+   	 System.out.println("모집기간: "); String period = input1.nextLine(); addNotice.add("모집조건");  // 6번
    	addNotice.add("모집기간: "+period); // 7번
-   	 System.out.println("경력: ");
-   	 String career = input1.nextLine();
-   	 addNotice.add("경력: "+career);  // 8번
-   	 System.out.println("성별: ");
-   	 String gender = input1.nextLine();
-   	 addNotice.add("성별: "+gender);  // 9번
-   	 System.out.println("나이: ");
-   	 String age = input1.nextLine();
-   	 addNotice.add("나이: "+age);  // 10번
-   	 System.out.println("학력: ");
-   	 String academic = input1.nextLine();
-   	 addNotice.add("학력: "+academic); // 11번
+   	 System.out.println("경력: "); String career = input1.nextLine(); addNotice.add("경력: "+career);  // 8번
+   	 System.out.println("성별: "); String gender = input1.nextLine(); addNotice.add("성별: "+gender);  // 9번
+   	 System.out.println("나이: "); String age = input1.nextLine(); addNotice.add("나이: "+age);  // 10번
+   	 System.out.println("학력: "); String academic = input1.nextLine(); addNotice.add("학력: "+academic); // 11번
    	 System.out.println("근무조건");
-   	 System.out.println("급여: ");
-   	 String salary = input1.nextLine(); 
-   	addNotice.add("근무조건"); // 12번
+   	 System.out.println("급여: ");  String salary = input1.nextLine();  addNotice.add("근무조건"); // 12번
    	 addNotice.add("급여: "+salary);  //13번
-   	 System.out.println("근무기간: ");
-   	 String period2= input1.nextLine();
-   	  addNotice.add("근무기간: "+period2); // 14번
-   	 System.out.println("근무요일: ");
-   	 String day= input1.nextLine(); 
-   	addNotice.add("근무요일: "+day); // 15번
-   	 System.out.println("근무시간: ");
-   	 String time= input1.nextLine();
-   	addNotice.add("근무시간: "+time);  // 16번
+   	 System.out.println("근무기간: "); String period2= input1.nextLine(); addNotice.add("근무기간: "+period2); // 14번
+   	 System.out.println("근무요일: ");  String day= input1.nextLine(); addNotice.add("근무요일: "+day); // 15번
+   	 System.out.println("근무시간: "); String time= input1.nextLine(); addNotice.add("근무시간: "+time);  // 16번
    	 System.out.println("모집내용");
-   	 System.out.println("모집직종: ");
-   	 String type1= input1.nextLine();  
-   	addNotice.add("모집내용");  // 17번
+   	 System.out.println("모집직종: "); String type1= input1.nextLine(); addNotice.add("모집내용");  // 17번
    	addNotice.add("모집직종: "+type1); // 18번
-   	 System.out.println("모집인원: ");
-   	 String personnel= input1.nextLine();
-   	addNotice.add("모집인원: "+personnel);  // 19번
-   	 System.out.println("고용형태: ");
-   	 String type2= input1.nextLine();
-   	addNotice.add("고용형태: "+type2); // 20번
-   	 System.out.println("우대조건: ");
-   	 String condition1= input1.nextLine(); 
-   	addNotice.add("우대조건: "+condition1);// 21번
-   	 System.out.println("기타조건: ");
-   	 String condition2= input1.nextLine();;
-   	addNotice.add("기타조건: "+condition2); // 22번
+   	 System.out.println("모집인원: "); String personnel= input1.nextLine(); addNotice.add("모집인원: "+personnel);  // 19번
+   	 System.out.println("고용형태: "); String type2= input1.nextLine(); addNotice.add("고용형태: "+type2); // 20번
+   	 System.out.println("우대조건: "); String condition1= input1.nextLine(); addNotice.add("우대조건: "+condition1);// 21번
+   	 System.out.println("기타조건: "); String condition2= input1.nextLine(); addNotice.add("기타조건: "+condition2); // 22번
    	 System.out.println("기타사항");
-   	 System.out.println("지원방법: ");
-   	 String way= input1.nextLine();
-   	addNotice.add("기타사항");  //23번
+   	 System.out.println("지원방법: "); String way= input1.nextLine(); addNotice.add("기타사항");  //23번
    	addNotice.add("지원방법: "+way); // 24번
-   	 System.out.println("제출서류: ");
-   	 String submission= input1.nextLine();
-   	addNotice.add("제출서류: "+submission); // 25번
-   	 System.out.println("코멘트: ");
-   	 String comment= input1.nextLine();
-   	 addNotice.add("코멘트: "+comment); // 26번   	
+   	 System.out.println("제출서류: "); String submission= input1.nextLine(); addNotice.add("제출서류: "+submission); // 25번
+   	 System.out.println("코멘트: ");  String comment= input1.nextLine(); addNotice.add("코멘트: "+comment); // 26번   	
 	 return addNotice; // 공고 작성이 끝나면 공고를 반환한다. 
 	 }	 
 	 public static void DeleteNotice(ArrayList<String> Notice) { // 
