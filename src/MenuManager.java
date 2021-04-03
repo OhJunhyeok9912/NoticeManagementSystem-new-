0cc506fc79dea8060a7f4e23f1988aefef489d4e
OhJunhyeok9912 <joonlove1234@gmail.com>
2021-04-03 21:23:51 +0900

    클래스를 이용한 메뉴실행 구현

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,201 +2,35 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MenuManager {
-    
-	public static ArrayList<String> AddNotice() {
-	 ArrayList<String> addNotice = new ArrayList<String>(); // 총 26개의 문자열이 저장됨. 
-     Scanner input = new Scanner(System.in);
-     Scanner input1 = new Scanner(System.in);
-     System.out.println("공고 넘버: (순서대로 입력하세요.)"); 
-   	 String NoticeNumber = input.next();  
-   	 addNotice.add("공고 넘버: "+NoticeNumber); // 1번
-     System.out.println("공고제목: ");
-   	 String NoticeTitle = input1.nextLine();  
-   	 addNotice.add("공고제목: "+NoticeTitle); //2번
-   	 System.out.println("근무지 정보");   
-   	 System.out.println("회사명: ");
-   	 String CompanyName = input1.nextLine();
-   	 addNotice.add("근무지 정보"); // 3번
-   	 addNotice.add("회사명: "+CompanyName); // 4번
-   	 System.out.println("근무지 위치: ");
-   	 String location = input1.nextLine();
-   	 addNotice.add("근무지 위치: "+location); // 5번
-   	 System.out.println("모집조건");
-   	 System.out.println("모집기간: ");
-   	 String period = input1.nextLine();  
-   	addNotice.add("모집조건");  // 6번
-   	addNotice.add("모집기간: "+period); // 7번
-   	 System.out.println("경력: ");
-   	 String career = input1.nextLine();
-   	 addNotice.add("경력: "+career);  // 8번
-   	 System.out.println("성별: ");
-   	 String gender = input1.nextLine();
-   	 addNotice.add("성별: "+gender);  // 9번
-   	 System.out.println("나이: ");
-   	 String age = input1.nextLine();
-   	 addNotice.add("나이: "+age);  // 10번
-   	 System.out.println("학력: ");
-   	 String academic = input1.nextLine();
-   	 addNotice.add("학력: "+academic); // 11번
-   	 System.out.println("근무조건");
-   	 System.out.println("급여: ");
-   	 String salary = input1.nextLine(); 
-   	addNotice.add("근무조건"); // 12번
-   	 addNotice.add("급여: "+salary);  //13번
-   	 System.out.println("근무기간: ");
-   	 String period2= input1.nextLine();
-   	  addNotice.add("근무기간: "+period2); // 14번
-   	 System.out.println("근무요일: ");
-   	 String day= input1.nextLine(); 
-   	addNotice.add("근무요일: "+day); // 15번
-   	 System.out.println("근무시간: ");
-   	 String time= input1.nextLine();
-   	addNotice.add("근무시간: "+time);  // 16번
-   	 System.out.println("모집내용");
-   	 System.out.println("모집직종: ");
-   	 String type1= input1.nextLine();  
-   	addNotice.add("모집내용");  // 17번
-   	addNotice.add("모집직종: "+type1); // 18번
-   	 System.out.println("모집인원: ");
-   	 String personnel= input1.nextLine();
-   	addNotice.add("모집인원: "+personnel);  // 19번
-   	 System.out.println("고용형태: ");
-   	 String type2= input1.nextLine();
-   	addNotice.add("고용형태: "+type2); // 20번
-   	 System.out.println("우대조건: ");
-   	 String condition1= input1.nextLine(); 
-   	addNotice.add("우대조건: "+condition1);// 21번
-   	 System.out.println("기타조건: ");
-   	 String condition2= input1.nextLine();;
-   	addNotice.add("기타조건: "+condition2); // 22번
-   	 System.out.println("기타사항");
-   	 System.out.println("지원방법: ");
-   	 String way= input1.nextLine();
-   	addNotice.add("기타사항");  //23번
-   	addNotice.add("지원방법: "+way); // 24번
-   	 System.out.println("제출서류: ");
-   	 String submission= input1.nextLine();
-   	addNotice.add("제출서류: "+submission); // 25번
-   	 System.out.println("코멘트: ");
-   	 String comment= input1.nextLine();
-   	 addNotice.add("코멘트: "+comment); // 26번   	
-	 return addNotice; // 공고 작성이 끝나면 공고를 반환한다. 
-	 }	 
-	 public static void DeleteNotice(ArrayList<String> Notice) { // 
-		Scanner input = new Scanner(System.in);
-		System.out.println("삭제하기 위한 공고 넘버를 입력하세요: "); 
-		String Num = input.nextLine();
-		int i=0;
-		int k=0;
-		NOnotice(Notice, Num);
-		if(Notice.size()>0 && (Notice.contains("공고 넘버: "+Num))) {
-			while(i<=Notice.size()) {
-				if((Notice.get(i)).equals("공고 넘버: "+Num)) {
-					for(int j = i;k<26;k++) { // j번 인덱스에서 remove를 하면 인덱스의 정보는 사라지고 그 뒤의 정보가 앞으로 당겨지므로 인덱스 j에서 26번 공고의 세부내용을 삭제해야 한다.  
-						Notice.remove(j);
-					}
-					break;	
-				}
-				else i++;
-			}	 
-		    System.out.println("삭제 되었습니다."); 	
-		}
-	 } 	 
-	 public static void EditNotice(ArrayList<String> Notice) {
-		Scanner input = new Scanner(System.in);
-		Scanner input1 = new Scanner(System.in);
-		System.out.println("편집하기 위한 공고 넘버를 입력하세요: "); 
-		String Num = input.nextLine();
-		int i = 0;
-		NOnotice(Notice, Num);
-		if(Notice.size()>0 && (Notice.contains("공고 넘버: "+Num))) {
-			while(i<=Notice.size()) {
-				if((Notice.get(i)).equals("공고 넘버: "+Num)) {
-					System.out.println("편집할 항목의 번호를 입력하세요. 번호를 잘 확인하세요"); 
-					System.out.println("2: 공고제목 4: 회사명, 5: 근무지 위치, 7: 모집기간, 8: 경력, 9: 성별, 10: 나이 , 11: 학력, 13: 급여, 14: 근무기간" );
-					System.out.println("15: 근무요일, 16: 근무시간 18: 모집직종, 19: 모집인원, 20: 고용형태, 21: 우대조건, 22: 기타조건, 24: 지원방법 , 25: 제출서류, 26: 코멘트" );
-					int Editnumber = input.nextInt();
-					System.out.println("편집할 내용을 입력하세요."); 
-					String Edit = input1.nextLine();
-					Notice.remove(i+(Editnumber-1));
-					switch(Editnumber){
-						case 2: Notice.add(i+(Editnumber-1), "공고제목: "+Edit); break;   case 4: Notice.add(i+(Editnumber-1), "회사명: "+Edit); break;
-						case 5: Notice.add(i+(Editnumber-1), "근무지 위치: "+Edit); break; case 7: Notice.add(i+(Editnumber-1), "모집기간: "+Edit); break;
-						case 8: Notice.add(i+(Editnumber-1), "경력: "+Edit); break;      case 9: Notice.add(i+(Editnumber-1), "성별: "+Edit); break;
-						case 10: Notice.add(i+(Editnumber-1), "나이: "+Edit); break;     case 11: Notice.add(i+(Editnumber-1), "학력: "+Edit); break;
-						case 13: Notice.add(i+(Editnumber-1), "급여: "+Edit); break;     case 14: Notice.add(i+(Editnumber-1), "근무기간: "+Edit); break;
-						case 15: Notice.add(i+(Editnumber-1), "근무요일: "+Edit); break;  case 16: Notice.add(i+(Editnumber-1), "근무시간: "+Edit); break;
-						case 18: Notice.add(i+(Editnumber-1), "모집직종: "+Edit); break;  case 19: Notice.add(i+(Editnumber-1), "모집인원: "+Edit); break;
-						case 20: Notice.add(i+(Editnumber-1), "고용형태: "+Edit); break;  case 21: Notice.add(i+(Editnumber-1), "우대조건: "+Edit); break;
-						case 22: Notice.add(i+(Editnumber-1), "기타조건: "+Edit); break;  case 24: Notice.add(i+(Editnumber-1), "지원방법: "+Edit); break;
-						case 25: Notice.add(i+(Editnumber-1), "제출서류: "+Edit); break;  case 26: Notice.add(i+(Editnumber-1), "코멘트: "+Edit); break;
-					}
-					break;	
-				}
-				else i++;
-			}
-			System.out.println("편집 되었습니다.");
-		}
-	 }	 
-	 public static void ViewNotice(ArrayList<String> Notice){
-		Scanner input = new Scanner(System.in);
-		ArrayList<String> viewNotice = new ArrayList<String>();
-		System.out.println("보길 원하는 공고 넘버를 입력하세요: ");
-		String Num = input.nextLine();
-		int i = 0;
-		NOnotice(Notice, Num);
-		if(Notice.size()>0 && (Notice.contains("공고 넘버: "+Num))) {
-			while(i<=Notice.size()) {
-				if((Notice.get(i)).equals("공고 넘버: "+Num)) {
-					for(int j = i;j<(26+i);j++) {  // 찾는 공고의 범위 만큼(1개의 공고 => 26개 문자열이 저장) 출력
-						System.out.println(Notice.get(j));
-					}
-					break;	
-				}
-				else i++;
-			}
-		}
-  }	 
-	 public static void NOnotice(ArrayList<String> Notice, String Num) { // 찾는 공고 넘버가 없을 때 또는 작성한 공고가 아무 것도 없을 때 사용
-		if(Notice.size()==0) { // 작성한 공고가 아무것도 없을 때 출력
-				System.out.println("공고가 없습니다!");
-		}
-		else if(Notice.size()>0 && !(Notice.contains("공고 넘버: "+Num))){ // 작성한 공고가 있으나 해당 넘버의 공고가 없을 때 출력
-				System.out.println("공고가 없습니다!");
-		}
-	 }
-	 
+ 
 	 public static void main(String[] args) {
-		 int num = 0;
+		 String num = "0";
 		 Scanner input = new Scanner(System.in);
-		 ArrayList<String> Notice = new ArrayList<String>();
-		 int i = 0;
-		 while(num != 6 ) {
+		 ArrayList<Notice> Notice = new ArrayList<>();
+		 NoticeManager noticemanager = new NoticeManager(input);
+		 while(!num.equals("5")) {
 			 System.out.println("*** Notice Management System Menu ***");
 		     System.out.println("1. Add Notice");
 		     System.out.println("2. Delete Notice");
 		     System.out.println("3. Edit Notice");
 		     System.out.println("4. View Notice");
-		     System.out.println("5. Show a Menu");
-		     System.out.println("6. Exit");
-		     System.out.println("Select one number between 1 - 6"); 
-		     num = input.nextInt();  
+		     System.out.println("5. Exit");
+		     System.out.println("Select one number between 1 - 5"); 
+		     num = input.nextLine();  
 		     switch(num) {
-		         case 1:
-		        	    Notice.addAll(AddNotice());
+		         case "1":
+		        	    Notice.addAll(noticemanager.AddNotice());
 		        	    break;
-		         case 2:
-		        	    DeleteNotice(Notice);
+		         case "2":
+		        	    noticemanager.DeleteNotice(Notice);
 	        	        break;
-		         case 3:
-		        	    EditNotice(Notice);
+		         case "3":
+		        	    noticemanager.EditNotice(Notice);
 	        	        break;
-		         case 4:
-		        	    ViewNotice(Notice);
+		         case "4":
+		        	    noticemanager.ViewNotice(Notice);
 	        	        break;
-		         case 5:
-	        	        break;
-		         case 6:
+		         case "5":
 	        	        break;
 	        	 default:
 	        		    System.out.println("Warning!: Select one number between 1 - 6\n");
---
+++ src/NoticeManager.java
@@ -0,0 +1,123 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NoticeManager {
+	Scanner input, input1;
+	
+	NoticeManager(Scanner input){
+		this.input = input;
+	}
+	
+	Notice notice;
+	public ArrayList<Notice> AddNotice() {
+		 notice = new Notice();
+		 ArrayList<Notice> addNotice = new ArrayList<>(); // Notice타입의 arrayList에 새로 추가한 공고를 넣는다. 
+	     System.out.println("공고 넘버: (순서대로 입력하세요.)"); notice.NoticeNumber = input.nextLine(); 
+	     System.out.println("공고제목: "); notice.NoticeTitle = input.nextLine();
+	   	 System.out.println("근무지 정보");   
+	   	 System.out.println("회사명: "); notice.CompanyName = input.nextLine(); 
+	   	 System.out.println("근무지 위치: "); notice.location = input.nextLine(); 
+	   	 System.out.println("모집조건");
+	   	 System.out.println("모집기간: "); notice.period = input.nextLine(); 
+	   	 System.out.println("경력: ");  notice.career = input.nextLine();
+	   	 System.out.println("성별: "); notice.gender = input.nextLine(); 
+	   	 System.out.println("나이: "); notice.age = input.nextLine(); 
+	   	 System.out.println("학력: "); notice.academic = input.nextLine(); 
+	   	 System.out.println("근무조건");
+	   	 System.out.println("급여: ");  notice.salary = input.nextLine();  
+	   	 System.out.println("근무기간: "); notice.period2= input.nextLine(); 
+	   	 System.out.println("근무요일: "); notice.day= input.nextLine();
+	   	 System.out.println("근무시간: "); notice.time= input.nextLine(); 
+	   	 System.out.println("모집내용");
+	   	 System.out.println("모집직종: "); notice.type1= input.nextLine();
+	   	 System.out.println("모집인원: "); notice.personnel= input.nextLine();
+	   	 System.out.println("고용형태: "); notice.type2= input.nextLine(); 
+	   	 System.out.println("우대조건: "); notice.condition1= input.nextLine(); 
+	   	 System.out.println("기타조건: "); notice.condition2= input.nextLine(); 
+	   	 System.out.println("기타사항");
+	   	 System.out.println("지원방법: "); notice.way= input.nextLine();
+	   	 System.out.println("제출서류: "); notice.submission= input.nextLine(); 
+	   	 System.out.println("코멘트: ");  notice.comment= input.nextLine();   	
+	   	 notice.setnotice(notice.NoticeNumber,notice.NoticeTitle ,notice.CompanyName, notice.location, notice.period , notice.career,notice.gender,
+	   	                   notice.age,notice.academic,notice.salary,notice.period2,notice.day,notice.time,notice.type1,notice.personnel,notice.type2,
+	   	                notice.condition1,notice.condition2,notice.way,notice.submission,notice.comment);
+	   	 addNotice.add(notice);	
+		 return addNotice; // 공고 작성이 끝나면 공고를 반환한다. 
+		 }	 
+	
+		 public void DeleteNotice(ArrayList<Notice> Notice) { // 
+			Notice deleteNotice;
+			System.out.println("삭제하기 위한 공고 넘버를 입력하세요: "); 
+			String Num = input.nextLine();
+			if(Notice.size() == 0) {
+				System.out.println("공고가 없습니다.");
+			}
+
+			else if(Notice.size()>0) {
+			for(int i=0; i<Notice.size();i++) {
+				deleteNotice = Notice.get(i);
+				if(Num.equals(deleteNotice.NoticeNumber)) {
+					  Notice.remove(i);
+					  System.out.println("공고가 삭제되었습니다!");
+				}
+			}
+			}
+		 } 	 
+		 
+		 public void EditNotice(ArrayList<Notice> Notice) {
+			Notice editNotice;
+			System.out.println("편집하기 위한 공고 넘버를 입력하세요: "); 
+			String Num = input.nextLine();
+			if(Notice.size() == 0) {
+				System.out.println("공고가 없습니다.");
+			}
+			else if(Notice.size()>0) {
+			for(int i=0; i<Notice.size();i++) {
+				editNotice = Notice.get(i);
+				if(Num.equals(editNotice.NoticeNumber)) {
+						System.out.println("편집할 항목의 번호를 입력하세요. 번호를 잘 확인하세요"); 
+						System.out.println("1: 공고제목 2: 회사명, 3: 근무지 위치, 4: 모집기간, 5: 경력, 6: 성별, 7: 나이 , 8: 학력, 9: 급여, 10: 근무기간" );
+						System.out.println("11: 근무요일, 12: 근무시간 13: 모집직종, 14: 모집인원, 15: 고용형태, 16: 우대조건, 17: 기타조건, 18: 지원방법 , 19: 제출서류, 20: 코멘트" );
+						String Editnumber = input.nextLine();
+						System.out.println("편집할 내용을 입력하세요."); 
+						String Edit = input.nextLine();
+						switch(Editnumber){   
+						     case "1": editNotice.NoticeTitle = Edit; break; case "2": editNotice.CompanyName = Edit; break;
+						     case "3": editNotice.location = Edit; break; case "4": editNotice.period = Edit; break;
+						     case "5": editNotice.career = Edit; break; case "6": editNotice.gender = Edit; break;
+						     case "7": editNotice.age = Edit; break; case "8": editNotice.academic = Edit; break;
+						     case "9": editNotice.salary = Edit; break; case "10": editNotice.period2 = Edit; break;
+						     case "11": editNotice.day = Edit; break; case "12": editNotice.time = Edit; break;
+						     case "13": editNotice.type1 = Edit; break; case "14": editNotice.personnel = Edit; break;
+						     case "15": editNotice.type2 = Edit; break; case "16": editNotice.condition1 = Edit; break;
+						     case "17": editNotice.condition2 = Edit; break; case "18": editNotice.way = Edit; break;
+						     case "19": editNotice.submission = Edit; break; case "20": editNotice.comment = Edit; break;
+							
+						}	
+						System.out.println("편집 되었습니다.");
+					}
+
+				}
+			} 
+		 }
+		 public void ViewNotice(ArrayList<Notice> Notice){
+			Notice viewNotice;
+			System.out.println("보길 원하는 공고 넘버를 입력하세요: ");
+			String Num = input.nextLine();
+			if(Notice.size() == 0) {
+				System.out.println("공고가 없습니다.");
+			}
+			else if(Notice.size()>0) {
+			for(int i=0; i<Notice.size();i++) {
+				viewNotice = Notice.get(i);
+				if(Num.equals(viewNotice.NoticeNumber)) {
+					  viewNotice.printNotice();
+				}
+				else if (viewNotice.NoticeNumber == null) {
+					System.out.println("공고가 없습니다.");
+				}
+			}
+			}
+	  }	 
+		
+}
