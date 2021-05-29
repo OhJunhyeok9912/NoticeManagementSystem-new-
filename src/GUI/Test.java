535340dc4777a81ca28329556e50a50984f136ac
OhJunhyeok9912 <80148751+OhJunhyeok9912@users.noreply.github.com>
2021-05-29 22:49:29 +0900

    Delete Test.java

--- src/GUI/Test.java
+++
@@ -1,8 +0,0 @@
-package GUI;
-
-public class Test {
-    public static void main(String[] args) {
-    	//MenuSelection menuselection = new MenuSelection();
-    	NoticeAdder studentadder = new NoticeAdder();
-    }
-}
